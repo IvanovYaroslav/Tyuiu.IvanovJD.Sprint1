41780cad4f7029442a4502f434364bbec7ac0819
Tyuiu.IvanovJD@outlook.com <yarikiro@mail.ru>
2023-10-09 16:59:25 +0300

    Task2

---
+++ Tyuiu.IvanovJD.Sprint1.Task2.V5.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.IvanovJD.Sprint1.Task2.V5.Lib
+{
+    public class DataService : ISprint1Task2V5
+    {
+        public int CalculateSideSquare(int a)
+        {
+            return 6 * (a * a);
+        }
+    }
+}
---
+++ Tyuiu.IvanovJD.Sprint1.Task2.V5.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.IvanovJD.Sprint1.Task2.V5.Lib;
+
+
+namespace Tyuiu.IvanovJD.Sprint1.Task1.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+
+            int a = 10;
+
+
+            int res = ds.CalculateSideSquare(a);
+            Assert.AreEqual(res, 600);
+        }
+    }
+}
