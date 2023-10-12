3811517d38fb78bf4380e78a623864b6711a0c91
Tyuiu.IvanovJD@outlook.com <yarikiro@mail.ru>
2023-10-12 11:53:55 +0300

    Task5

---
+++ Tyuiu.IvanovJD.Sprint1.Task5.V2.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.IvanovJD.Sprint1.Task5.V2.Lib;
+
+namespace Tyuiu.IvanovJD.Sprint1.Task5.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double temp = 32;
+            DataService ds = new DataService();
+
+            var res = ds.FahrenheitToСelsius(temp);
+
+            int result = Convert.ToInt32(res);
+
+            int wait = 0;
+
+            Assert.AreEqual(wait, result);
+
+
+
+        }
+    }
+}
---
+++ Tyuiu.IvanovJD.Sprint1.Task5.V2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.IvanovJD.Sprint1.Task5.V2.Lib;
+namespace Tyuiu.IvanovJD.Sprint1.Task5.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Иванов Я. Д. | ПКТБ-23-2";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                *");
+            Console.WriteLine("* Тема: Базовые навыки работы в С#                                         *");
+            Console.WriteLine("* Задание #5                                                               *");
+            Console.WriteLine("* Вариант #2                                                               *");
+            Console.WriteLine("* Выполнил: Иванов Ярослав Дмитриевич | ПКТБ-23-2                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные,  *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.              *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+
+            double temp;
+            Console.WriteLine("Введите значение градусов в Фаренгейтах: ");
+            temp = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            int tempT = Convert.ToInt32(ds.FahrenheitToСelsius(temp));
+            Console.WriteLine("Температура в градусах: " + tempT);
+
+            Console.ReadKey();
+        }
+    }
+}
